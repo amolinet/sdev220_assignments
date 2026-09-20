58407ce94f41acd8a66eb8eb9c34779db038f789
amolinet <abibbler@gmail.com>
2026-09-20 17:58:19 -0500

    followed tutorial and updated
    updated to fit assignment instructions
    After watching the video, create a CRUD API for a Book instead of a Drink in the video example above.  The Book model should have the following parameters:
    id
    book_name
    author
    publisher
    
    attached screenshot of api working

---
+++ application.py
@@ -0,0 +1,66 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+
+db = SQLAlchemy(app)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    book_name = db.Column(db.String(120), unique=True, nullable=False)
+    author = db.Column(db.String(80))
+    publisher = db.Column(db.String(80))
+    
+    def __repr__(self):
+        return f'{self.id}-{self.book_name}-{self.author}-{self.publisher}'
+
+@app.route('/')
+def index():
+    return 'Hello! Here you can save books to a dictionary!'
+
+# 1. GET ALL BOOKS
+@app.route('/books')
+def get_books():
+    books = Book.query.all()
+    output = []
+    for book in books:
+        book_data = {
+            'id': book.id,
+            'book_name': book.book_name, 
+            'author': book.author, 
+            'publisher': book.publisher
+        }
+        output.append(book_data)
+    return {'books': output}
+
+# 2. GET A SINGLE BOOK BY ID
+@app.route('/books/<id>')
+def get_book(id):
+    book = Book.query.get_or_404(id)
+    return {
+        'book_name': book.book_name, 
+        'author': book.author, 
+        'publisher': book.publisher
+    }
+
+# 3. ADD A NEW BOOK (POST)
+@app.route('/books', methods=["POST"])
+def add_book():
+    book = Book(
+        book_name=request.json['book_name'], 
+        author=request.json['author'], 
+        publisher=request.json['publisher']
+    )
+    db.session.add(book)
+    db.session.commit()
+    return {'id': book.id}
+
+# 4. DELETE A BOOK
+@app.route('/books/<id>', methods=['DELETE'])
+def delete_book(id):
+    book = Book.query.get(id)
+    if book is None:
+        return {'error': 'not found'}
+    db.session.delete(book)
+    db.session.commit()
+    return {'message': 'yeet... book deleted!'}
